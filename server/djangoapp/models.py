3c956c26de1865664fc8ca440f894434963f38e9
wildweazel7 <82596606+wildweazel7@users.noreply.github.com>
2024-02-29 16:05:38 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -13,14 +13,18 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # - __str__ method to print a car make object
 class CarMake(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField(help_text=
-                                   "Any useful information about maker")
-    founded_year = models.IntegerField(help_text=
-                                       "Year the company was founded")
-    headquarters = models.CharField(max_length=255, 
-                                    help_text="Location of the company's headquarters")
-    website = models.URLField(max_length=200, blank=True, 
-                              help_text="Company's official website")
+    description = models.TextField(
+        help_text="Any useful information about maker"
+    )
+    founded_year = models.IntegerField(
+        help_text="Year the company was founded"
+    )
+    headquarters = models.CharField(
+        max_length=255, help_text="Location of the company's headquarters"
+    )
+    website = models.URLField(
+        max_length=200, blank=True, help_text="Company's official website"
+    )
 
 
     def __str__(self):
@@ -60,25 +64,32 @@ class CarModel(models.Model):
         ('WAGON', 'Wagon')
     ]
        
-    name = models.CharField(max_length=255, unique=True, help_text="Name of model")
-    model_type = models.CharField(max_length=50, choices=mTypeCoice, default='SUV')
+    name = models.CharField(
+        max_length=255, unique=True, help_text="Name of model"
+    )
+    model_type = models.CharField(
+        max_length=50, choices=mTypeCoice, default='SUV'
+    )
     year = models.IntegerField(default=2023,
         validators=[
             MaxValueValidator(2024),
             MinValueValidator(2015)
         ])
-    car_make = models.ForeignKey\
-    (CarMake, on_delete=models.CASCADE, related_name='car_models')
-    color = models.CharField\
-    (max_length=255, choices=mColorChoice, help_text="Offered colors of the model")
-    doors = models.CharField\
-    (max_length=255, choices=mDoorChoice, 
-     help_text="Number of doors possible", default="Five")
-    extras = models.CharField\
-    (max_length=255, choices=mExtraChoice, 
-     help_text="List of extras",blank=True, default='NONE')
-    price = models.DecimalField\
-    (max_digits=10, decimal_places=2, help_text="Price in USD",blank=True, default=10000)
+    car_make = models.ForeignKey(
+        CarMake, on_delete=models.CASCADE, related_name='car_models'
+    )
+    color = models.CharField(
+        max_length=255, choices=mColorChoice, help_text="Offered colors of the model"
+    )
+    doors = models.CharField(
+        max_length=255, choices=mDoorChoice, help_text="Number of doors possible", default="Five"
+    )
+    extras = models.CharField(
+        max_length=255, choices=mExtraChoice, help_text="List of extras",blank=True, default='NONE'
+    )
+    price = models.DecimalField(
+        max_digits=10, decimal_places=2, help_text="Price in USD",blank=True, default=10000
+    )
     dealer_id = models.IntegerField(blank=True,null=True,default=1)
 
     def __str__(self):
