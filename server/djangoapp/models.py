160f6a3313ba6b0acd70575505662c4c56309d85
wildweazel7 <82596606+wildweazel7@users.noreply.github.com>
2024-02-29 15:41:20 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -14,14 +14,15 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 # - __str__ method to print a car make object
 class CarMake(models.Model):
     name = models.CharField(max_length=255, unique=True)
-    description = models.TextField\
-    (help_text="Any useful information about maker")
-    founded_year = models.IntegerField\
+    description = \ 
+    models.TextField(help_text="Any useful information about maker")
+    founded_year = models.IntegerField \
     (help_text="Year the company was founded")
-    headquarters = models.CharField\
-    (max_length=255, help_text="Location of the company's headquarters")
-    website = models.URLField\
-    (max_length=200, blank=True, help_text="Company's official website")
+    headquarters = models.CharField(max_length=255, \
+    help_text="Location of the company's headquarters")
+    website = models.URLField(max_length=200, blank=True, \ 
+    help_text="Company's official website")
+
 
     def __str__(self):
         return self.name
@@ -37,27 +38,27 @@ class CarMake(models.Model):
 # - __str__ method to print a car make object
 class CarModel(models.Model):
     mColorChoice = [
-        ('RED',"Red"),
-        ('Blue', "Blue"),
-        ('Black', "Black") ,
-        ('White', "White"),
-        ('Green', "Green"),
+        ('RED',"Red"), 
+        ('Blue', "Blue"), 
+        ('Black', "Black"), 
+        ('White', "White"), 
+        ('Green', "Green"), 
         ('Yellow', "Yellow")
     ]
     mDoorChoice = [
-        ("Three","Three"),
+        ("Three","Three"), 
         ("Five","Five")
     ]
     mExtraChoice = [
         ("NONE","No Extra"),
-        ("SOUND","Sound system"),
-        ("AIR","Air-cooling-system"),
+        ("SOUND","Sound system"), 
+        ("AIR","Air-cooling-system"), 
         ("NAV", "Navigation system")
     ]
     mTypeCoice = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
+        ('SEDAN', 'Sedan'), 
+        ('SUV', 'SUV'), 
+        ('WAGON', 'Wagon')
     ]
        
     name = models.CharField(max_length=255, unique=True, help_text="Name of model")
@@ -67,13 +68,19 @@ class CarModel(models.Model):
             MaxValueValidator(2024),
             MinValueValidator(2015)
         ])
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE, related_name='car_models')
-    color = models.CharField(max_length=255, choices=mColorChoice, help_text="Offered colors of the model")
-    doors = models.CharField(max_length=255, choices=mDoorChoice, help_text="Number of doors possible", default="Five")
-    extras = models.CharField(max_length=255, choices=mExtraChoice, help_text="List of extras",blank=True, default='NONE')
-    price = models.DecimalField(max_digits=10, decimal_places=2, help_text="Price in USD",blank=True, default=10000)
+    car_make = models.ForeignKey\
+    (CarMake, on_delete=models.CASCADE, related_name='car_models')
+    color = models.CharField\
+    (max_length=255, choices=mColorChoice, help_text="Offered colors of the model")
+    doors = models.CharField\
+    (max_length=255, choices=mDoorChoice, help_text="Number of doors possible", default="Five")
+    extras = models.CharField\
+    (max_length=255, choices=mExtraChoice, help_text="List of extras",blank=True, default='NONE')
+    price = models.DecimalField\
+    (max_digits=10, decimal_places=2, help_text="Price in USD",blank=True, default=10000)
     dealer_id = models.IntegerField(blank=True,null=True,default=1)
 
     def __str__(self):
         # Returns the name of the car make when it's printed
-        return f"{self.car_make.name} {self.name} - {self.extras} - {self.color} - {self.doors} - {self.price}"
+        return f"{self.car_make.name} {self.name} - \
+        {self.extras} - {self.color} - {self.doors} - {self.price}"
